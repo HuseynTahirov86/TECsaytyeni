b53671f22ef453d26973c229cffc0a77f43bef78
Hüseyn Tahirov <huseyntahirov2@gmail.com>
2026-01-14 06:10:27 +0000

    Naxçıvan Dövlət Universiteti yan düşsün

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import Link from "next/link";
@@ -51,8 +52,7 @@ export function Header() {
           <Link href="/" className="flex items-center space-x-3">
              <Image src="/logo1.png" alt="TEC Təhlilləri Loqosu" width={60} height={60} className="object-contain flex-shrink-0" />
             <div className="hidden sm:flex flex-col text-sm font-semibold leading-tight min-w-0">
-              <span className="whitespace-nowrap text-xs lg:text-sm">Naxçıvan Dövlət Universiteti</span>
-              <span className="text-xs lg:text-sm font-medium">Tələbə Elmi Cəmiyyəti</span>
+              <span className="whitespace-nowrap text-xs lg:text-sm">Naxçıvan Dövlət Universiteti | Tələbə Elmi Cəmiyyəti</span>
             </div>
           </Link>
         </div>
